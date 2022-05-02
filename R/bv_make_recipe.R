c7fddaab31433212dae0a962e800bd12a306425e
bcjaeger <byron.jaeger@gmail.com>
2022-05-02 16:04:52 -0400

    simpler recipe without select

--- R/bv_make_recipe.R
+++ R/bv_make_recipe.R
@@ -9,44 +9,19 @@ bv_make_recipe <- function(bv_data,
                            impute = FALSE) {
 
   out <- recipe(bv_data, outcome ~ .) |>
-    step_select(
-      -any_of(
-        c("RIN",
-          "ppt_type",
-          "type",
-          "labelid",
-          "Viability",
-          "low_rin",
-          "low_viability",
-          "participantGUID",
-          "labelGUID",
-          "MTP_SMPBID",
-          "MTP_OGLABELID",
-          "MTP_REPOLABELID",
-          "pid",
-          "bid",
-          "recid",
-          "fuaddcom_pcaa",
-          "fuaddcom_pcab",
-          "recordthread_pcab",
-          "recordthread_pcaa",
-          "vialLabel",
-          "Live_cell_concentration")
-      ),
-      -contains('comments'),
-      -starts_with("collForm"),
-      -starts_with('notes'),
-      -starts_with("d_")) |>
     step_nzv(all_predictors()) |>
-    step_mutate(across(starts_with("t_"), hrs_since_midnight),
-                across(starts_with("staffID"), factor)) |>
-    step_unknown(all_nominal_predictors())
+    step_mutate(across(any_of(c("staffID",
+                                "barcodeID_pcaa",
+                                "barcodeID_pcab",
+                                "coll_staffid")), factor))
 
   if(impute)
     out <- out |>
       step_impute_median(all_numeric_predictors()) |>
       step_impute_mode(all_nominal_predictors())
-
+  else
+    out <- out |>
+      step_unknown(all_nominal_predictors())
 
   if(one_hot)
     out <- out |>
